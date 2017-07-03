fc9630ae2d96dfa577be1314805c00b91d566a0c
pomber <pombopombopombo@gmail.com>
2017-07-02 21:11:52 -0300

    Format leagues

--- web/src/data/leagues.js
+++ web/src/data/leagues.js
@@ -1,7 +1,58 @@
-export default [{
-  name: 'Premier League',
-  teams: ["Bournemouth","Chelsea","Everton","Leicester","Man United","Norwich","Arsenal","Newcastle","Stoke","West Brom","Aston Villa","Southampton","Sunderland","Swansea","Tottenham","Watford","West Ham","Crystal Palace","Man City","Liverpool","Burnley","Hull","Middlesbrough"],
-}, {
-  name: 'Serie A',
-  teams: ["Lazio","Verona","Empoli","Fiorentina","Frosinone","Inter","Juventus","Palermo","Sampdoria","Sassuolo","Bologna","Milan","Atalanta","Carpi","Chievo","Genoa","Napoli","Roma","Torino","Udinese","Pescara","Cagliari","Crotone"],
-}];
+export default [
+  {
+    name: "Premier League",
+    teams: [
+      "Bournemouth",
+      "Chelsea",
+      "Everton",
+      "Leicester",
+      "Man United",
+      "Norwich",
+      "Arsenal",
+      "Newcastle",
+      "Stoke",
+      "West Brom",
+      "Aston Villa",
+      "Southampton",
+      "Sunderland",
+      "Swansea",
+      "Tottenham",
+      "Watford",
+      "West Ham",
+      "Crystal Palace",
+      "Man City",
+      "Liverpool",
+      "Burnley",
+      "Hull",
+      "Middlesbrough"
+    ]
+  },
+  {
+    name: "Serie A",
+    teams: [
+      "Lazio",
+      "Verona",
+      "Empoli",
+      "Fiorentina",
+      "Frosinone",
+      "Inter",
+      "Juventus",
+      "Palermo",
+      "Sampdoria",
+      "Sassuolo",
+      "Bologna",
+      "Milan",
+      "Atalanta",
+      "Carpi",
+      "Chievo",
+      "Genoa",
+      "Napoli",
+      "Roma",
+      "Torino",
+      "Udinese",
+      "Pescara",
+      "Cagliari",
+      "Crotone"
+    ]
+  }
+];
