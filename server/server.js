a25ff694c9f020ffcc5612e1f18d5783003a9872
pomber <pombopombopombo@gmail.com>
2017-07-03 18:19:07 -0300

    Predict values from docker container

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 const app = express();
 
 function getMatchStats(leagueId, homeTeam, awayTeam) {
@@ -23,7 +24,17 @@ app.get("/api/:league/match", (req, res) => {
 
   const matchStats = getMatchStats(leagueId, home, away);
 
-  res.send({ home: 0.5, draw: 0.2, away: 0.3 });
+  axios
+    .post("http://localhost:4000/predictproba", matchStats)
+    .then(response => {
+      console.log(response.data);
+			const {H,D,A} = response.data[0]
+      res.send({home: H, draw: D, away: A});
+    })
+    .catch(error => {
+      console.log(error.response.data);
+      res.status(500).send({error: error.response.data});
+    });
 });
 
 app.listen(3000);
