6c9773a37ffcdfd7cf32cb1b1d8c73c316b8b4b6
pomber <pombopombopombo@gmail.com>
2017-07-05 13:50:39 -0300

    Add prod build

--- web/config/index.js
+++ web/config/index.js
@@ -27,7 +27,7 @@ module.exports = {
     autoOpenBrowser: false,
     assetsSubDirectory: "static",
     assetsPublicPath: "/",
-    proxyTable: {"/api": {target: "http://server:3000"}},
+    proxyTable: {"/api": {target: "http://server"}},
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
