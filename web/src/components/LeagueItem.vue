5662e34c82b562904a4dd8c2aaa518150c860842
Anicar <anicarrr@gmail.com>
2017-08-14 02:04:34 -0300

    add logoLeagues and modify LeagueItem component

--- web/src/components/LeagueItem.vue
+++ web/src/components/LeagueItem.vue
@@ -1,11 +1,27 @@
 <template>
-  <span>{{league.name}}</span>
+  <div>
+    <img :src="leagueLogo" />
+    <br />
+  	<span>{{league.name}}</span>
+  </div>
+  
 </template>
 
 <script>
+
+function getLogo(league) {
+	return `/static/img/leaguesLogos/${league}.png`
+}
+
 export default {
   name: 'league-item',
   props: ['league'],
+
+  data() {
+  	return {
+  		leagueLogo: getLogo(this.league.slug)
+  	}
+  }
 };
 </script>
 
